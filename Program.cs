63e56becdcf088b5016dcdbc48e376f7a2096ed1
SkerrHome <sker078@aucklanduni.ac.nz>
2020-03-04 18:17:59 +1300

    starting project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApiClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
